da9947aa41f69ef770abe71347d37fbc34111f32
Orlando Marquez <orlando.m.bol@gmail.com>
2018-11-27 23:49:16 -0500

    Added lines to unpack files

--- download_embeddings.sh
+++ download_embeddings.sh
@@ -2,3 +2,5 @@
 
 wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/word-vectors-v2/cc.eu.300.vec.gz
 wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki-news-300d-1M.vec.zip
+gunzip cc.eu.300.vec.gz
+python extract_zip.py wiki-news-300d-1M.vec
