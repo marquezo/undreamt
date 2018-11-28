0e5065dee0a188872056edc6f437db77b3dcb68e
Orlando Marquez <orlando.m.bol@gmail.com>
2018-11-27 23:23:21 -0500

    script to download word embeddings

---
+++ download_embeddings.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/word-vectors-v2/cc.eu.300.vec.gz
+wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki-news-300d-1M.vec.zip
