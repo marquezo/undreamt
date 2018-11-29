91398cad10bb7cd8706f2f77725ffc8533e376d7
Huynyda Bac <huynyda.bac@intact.net>
2018-11-29 17:44:55 -0500

    Fixed typo

--- download_embeddings.sh
+++ download_embeddings.sh
@@ -3,4 +3,4 @@
 wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/word-vectors-v2/cc.eu.300.vec.gz
 wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki-news-300d-1M.vec.zip
 gunzip cc.eu.300.vec.gz
-python extract_zip.py wiki-news-300d-1M.vec
+python extract_zip.py wiki-news-300d-1M.vec.zip
