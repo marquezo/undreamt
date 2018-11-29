ad1cb612eac99f2266c14c30df82cbce78dbd061
Orlando Marquez <orlando.m.bol@gmail.com>
2018-11-29 17:19:22 -0500

    Changing vocabulary size to 70k

--- prep_en_data.sh
+++ prep_en_data.sh
@@ -2,4 +2,4 @@
 
 wget http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2013.en.shuffled.gz
 gunzip news.2013.en.shuffled.gz
-python preprocess_corpus.py news.2013.en.shuffled vocab.en sentences.en en
+python preprocess_corpus.py news.2013.en.shuffled vocab.en sentences.en en --size_vocab 70000
