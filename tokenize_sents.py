634ee07c08fd6d8ad3a7f5688a2e2edde2a9e44b
Orlando Marquez <orlando.m.bol@gmail.com>
2018-12-09 16:29:31 -0500

    lowercased Berriak test data for consistent comparison

--- tokenize_sents.py
+++ tokenize_sents.py
@@ -4,30 +4,10 @@ BLEU score assessor
 """
 # -*- coding: utf-8 -*-
 import argparse
+from unicodedata import normalize
+import re, string
 from euskalToken import EuskalToken
 from nltk.tokenize.treebank import TreebankWordTokenizer
-
-
-def avg_of_sentence_bleu_scores(groundtruth_filename, prediction_filename):
-    
-    list_of_scores = []
-    basque_tokenizer = EuskalToken()
-    
-    with open(groundtruth_filename, encoding="utf8") as gtfile, open(prediction_filename, encoding="utf8") as predfile:
-   
-        for i, (gt_sentence, predicted_sentence) in enumerate(zip(gtfile, predfile)):   
-            gt_list_of_tokens = basque_tokenizer.tokenize(gt_sentence)
-            predicted_list_of_tokens = predicted_sentence.split()
-            print(gt_list_of_tokens)
-            print(predicted_list_of_tokens)
-            score = bleu_score.sentence_bleu([gt_list_of_tokens], predicted_list_of_tokens)
-            list_of_scores.append(score)
-            print(score)
-            
-        array_of_scores = np.asarray(list_of_scores)
-        avg_bleu_scores = array_of_scores.mean()
-          
-    return avg_bleu_scores
 
 
 def main():
@@ -38,13 +18,24 @@ def main():
     args = parser.parse_args()
 
     word_tokenizer = EuskalToken() if args.lang == 'eu' else TreebankWordTokenizer()
+    # prepare regex for char filtering
+    re_print = re.compile('[^%s]' % re.escape(string.printable))
 
     with open(args.file, 'r', encoding='utf8') as file:
         lines = file.readlines()
 
         for line in lines:
-            tokens = word_tokenizer.tokenize(line.rstrip())
-            print(" ".join(tokens))
+            line = line.rstrip()
+            # normalize unicode characters
+            line = normalize('NFD', line).encode('ascii', 'ignore')
+            line = line.decode('UTF-8')
+            # tokenize
+            line = word_tokenizer.tokenize(line)
+            # convert to lower case
+            line = [word.lower() for word in line]
+            # remove non-printable chars from each token
+            line = [re_print.sub('', w) for w in line]
+            print(" ".join(line))
 
 
 if __name__ == '__main__':
